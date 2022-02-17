a75c881018e96ae163d7dde957b744e4609bebed
Chandra <chandra.edouard@gmail.com>
2022-02-17 13:59:21 +0000

    Create countdown main.js

--- js/main.js
+++ js/main.js
@@ -4,21 +4,19 @@
 /* *********************************** DONNEES *************************************/
 /***********************************************************************************/
 
-const rocket = document.getElementById('rocket')
-const buttonFiring = document.getElementById('firing-button')
+const rocket = document.getElementById('rocket');
+const buttonFiring = document.getElementById('firing-button');
 // const buttonCancel = document.createElement('img')
 // buttonCancel.src = "images/cancel-button.png";
 
 
 /*Compte à reboure*/
 
-const chrono = document.getElementById('billboard')
+// const billboard = document.getElementById('billboard')
+const billboard = document.querySelector("#billboard span");
 
-// let time_minutes = 0; //  minutes
-// let time_seconds = 10; // seconds
-// let intervalID = window.setInterval(time_seconds, 100, time_seconds)
-
-
+let countDown = 10; 
+let intervalID ;
 
 
 /***********************************************************************************/
@@ -26,25 +24,30 @@ const chrono = document.getElementById('billboard')
 /***********************************************************************************/
 
 /*
- * clic bouton : deconte comence setinterval
- *               gif image change (2) querySelector
- *               image bouton change
+ * clic bouton : deconte comence setinterval ok
+ *               gif image change (2) ok
+ *               image bouton change ok
                  deconte fini gif image change (3)
 */
 
 
 buttonFiring.addEventListener('click', function(){
         buttonFiring.src = "images/cancel-button.png";
-        rocket.src = "images/rocket2.gif"
-        // function startCountdown(time_minutes, time_seconds){
-        //     time_seconds.innerText = time_seconds --;
-        //     console.log(startCountdown)
-        //     if (time_seconds ==0)
-        //     window.clearInterval(intervalID);
-        //     console.log('timer==0');
-        // } 
-  console.log('hello');
-})
+        rocket.src = "images/rocket2.gif";
+        
+        function startCountdown(){
+            
+            if (countDown <= 0){
+                clearInterval(intervalID);
+            }
+              billboard.innerHTML = countDown;
+              countDown--;
+        } 
+        
+        intervalID = setInterval(startCountdown, 1000);
+        
+//   console.log('hello');
+});
 
 
 
