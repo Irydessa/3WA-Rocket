37fcc756da7686637b7ea2c5af2d61a48aeb4c23
Chandra <chandra.edouard@gmail.com>
2022-02-17 13:20:07 +0000

    Create function main.js

--- js/main.js
+++ js/main.js
@@ -6,11 +6,17 @@
 
 const rocket = document.getElementById('rocket')
 const buttonFiring = document.getElementById('firing-button')
-const buttonCancel = document.createElement('img')
-buttonCancel.src = "./imgages/cancel-button.png";
+// const buttonCancel = document.createElement('img')
+// buttonCancel.src = "images/cancel-button.png";
+
+
+/*Compte à reboure*/
+
 const chrono = document.getElementById('billboard')
 
-
+// let time_minutes = 0; //  minutes
+// let time_seconds = 10; // seconds
+// let intervalID = window.setInterval(time_seconds, 100, time_seconds)
 
 
 
@@ -19,11 +25,27 @@ const chrono = document.getElementById('billboard')
 /* ********************************** FONCTIONS ************************************/
 /***********************************************************************************/
 
+/*
+ * clic bouton : deconte comence setinterval
+ *               gif image change (2) querySelector
+ *               image bouton change
+                 deconte fini gif image change (3)
+*/
+
 
 buttonFiring.addEventListener('click', function(){
-        buttonFiring.src = "./imgages/cancel-button.png";
+        buttonFiring.src = "images/cancel-button.png";
+        rocket.src = "images/rocket2.gif"
+        // function startCountdown(time_minutes, time_seconds){
+        //     time_seconds.innerText = time_seconds --;
+        //     console.log(startCountdown)
+        //     if (time_seconds ==0)
+        //     window.clearInterval(intervalID);
+        //     console.log('timer==0');
+        // } 
   console.log('hello');
 })
+
 
 
 
